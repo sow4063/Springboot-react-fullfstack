ac83076a66ae91264d443468e7d4f908a82cc50c
sow4063 <sow4063@gmail.com>
2022-06-30 10:32:02 +0900

    first commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import com.example.demo.student.exception.BadRequestException;
+import com.example.demo.student.exception.StudentNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Boolean existEmail = studentRepository.selectExistsEmail(student.getEmail());
+
+        if( existEmail ) {
+            throw new BadRequestException("Email" + student.getEmail() + " is taken.");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        if(!studentRepository.existsById(studentId)) {
+            //throw new StudentNotFoundException("Student with id " + studentId + " does not exists.");
+            throw new IllegalStateException("Student with id " + studentId + " does not exists.");
+        }
+        studentRepository.deleteById(studentId);
+    }
+}
