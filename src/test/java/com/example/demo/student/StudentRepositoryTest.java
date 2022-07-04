3f56924ef5e91e3f49658e3b21fa0502d1178fb4
jongik lee <sow4063@gmail.com>
2022-07-05 08:05:57 +0900

    Test it (#8)
    * add deploy
    
    * add testing
    
    * add testing#2

---
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    void itShouldCheckWhenStudentEmailExists() {
+        // given
+        String email = "jamila@gmail.com";
+        Student student = new Student(
+                "Jamila",
+                email,
+                Gender.FEMALE
+        );
+        underTest.save(student);
+
+        // when
+        boolean expected = underTest.selectExistsEmail(email);
+
+        // then
+        assertThat(expected).isTrue();
+    }
+
+    @Test
+    void itShouldCheckWhenStudentEmailDoesNotExists() {
+        // given
+        String email = "jamila@gmail.com";
+
+        // when
+        boolean expected = underTest.selectExistsEmail(email);
+
+        // then
+        assertThat(expected).isFalse();
+    }
+}
